9d152e94f1843f094f103a252c8ca7ad42802ec7
Atul Varma <varmaa@gmail.com>
2019-12-27 11:24:42 -0500

    Add export_spritesheet.py.

---
+++ export_spritesheet.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+
+ASE_FILENAME = "manhattan.aseprite"
+
+JSON_FILENAME = "manhattan.json"
+
+PNG_FILENAME = "manhattan.png"
+
+ASEPRITE_PATH = os.environ.get('ASEPRITE_PATH')
+
+ASEPRITE_ARGS = [
+    "-b",
+    "--data", JSON_FILENAME,
+    "--sheet", PNG_FILENAME,
+    "--split-layers",
+    ASE_FILENAME,
+]
+
+ASEPRITE_PATHS_TO_TRY = [
+    Path("C:\\") / "Program Files" / "Aseprite" / "Aseprite.exe", 
+]
+
+
+def find_aseprite() -> Path:
+    if ASEPRITE_PATH:
+        return Path(ASEPRITE_PATH)
+    for path in ASEPRITE_PATHS_TO_TRY:
+        if path.exists():
+            return path
+    raise Exception(f'Aseprite not found, please define ASEPRITE_PATH in your environment!')
+
+
+def main():
+    print(f"Exporting {ASE_FILENAME} to {JSON_FILENAME} and {PNG_FILENAME}...")
+    if subprocess.call([find_aseprite(), *ASEPRITE_ARGS]):
+        sys.exit(1)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
