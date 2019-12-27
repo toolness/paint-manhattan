11d9fdc85f43098c896afc5c4ff56f59defc7f31
Atul Varma <varmaa@gmail.com>
2019-12-27 13:16:39 -0500

    Load image into canvas.

--- src/aseprite-sheet.ts
+++ src/aseprite-sheet.ts
@@ -33,7 +33,33 @@ type AsepriteSheetMetadata = {
 type AsepriteSheet = {
   metadata: AsepriteSheetMetadata,
   image: HTMLImageElement,
+  canvas: HTMLCanvasElement,
 };
+
+function loadImage(url: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.onerror = reject;
+    image.src = url;
+  });
+}
+
+function imageToCanvas(image: HTMLImageElement): HTMLCanvasElement {
+  const canvas = document.createElement('canvas');
+  canvas.height = image.naturalHeight;
+  canvas.width = image.naturalWidth;
+  const ctx = canvas.getContext('2d');
+  if (!ctx) {
+    throw new Error('Unable to get 2d context of canvas!');
+  }
+  ctx.drawImage(image, 0, 0);
+
+  // Verify that the canvas data isn't tainted.
+  ctx.getImageData(0, 120, 1, 1);
+
+  return canvas;
+}
 
 export async function loadAsepriteSheet(path: string): Promise<AsepriteSheet> {
   const baseURL = new URL(window.location.href);
@@ -44,11 +70,14 @@ export async function loadAsepriteSheet(path: string): Promise<AsepriteSheet> {
   }
   const metadata: AsepriteSheetMetadata = await metadataRes.json();
   const imageURL = new URL(metadata.meta.image, metadataURL).href;
+  const image = await loadImage(imageURL);
 
-  return new Promise((resolve, reject) => {
-    const image = new Image();
-    image.onload = () => resolve({metadata, image});
-    image.onerror = reject;
-    image.src = imageURL;
-  });
+  if (image.naturalWidth !== metadata.meta.size.w ||
+      image.naturalHeight !== metadata.meta.size.h) {
+    throw new Error('Assertion failure, image is not expected dimensions!');
+  }
+
+  const canvas = imageToCanvas(image);
+
+  return {metadata, image, canvas};
 }
