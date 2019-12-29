b7616abbdc46004a98a4a25f8be0e6c9bc4b80ad
Atul Varma <varmaa@gmail.com>
2019-12-29 16:56:47 -0500

    Export splash screen from aseprite too.

--- export_from_aseprite.py
+++ export_from_aseprite.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import List, Optional
 import subprocess
 from pathlib import Path
 
@@ -8,27 +9,32 @@ MY_DIR = Path(__file__).parent.resolve()
 
 GRAPHICS_DIR = MY_DIR / "graphics"
 
-ASE_FILENAME = "manhattan.aseprite"
-
-JSON_FILENAME = "manhattan.json"
-
-PNG_FILENAME = "manhattan.png"
-
 ASEPRITE_PATH = os.environ.get('ASEPRITE_PATH')
-
-# https://www.aseprite.org/docs/cli/
-ASEPRITE_ARGS = [
-    "-b",
-    "--data", JSON_FILENAME,
-    "--sheet", PNG_FILENAME,
-    "--split-layers",
-    "--filename-format", r"{layer}",
-    ASE_FILENAME,
-]
 
 ASEPRITE_PATHS_TO_TRY = [
     Path("C:\\") / "Program Files" / "Aseprite" / "Aseprite.exe", 
 ]
+
+
+def export(aseprite_binary: Path, basename: str, extra_args: Optional[List[str]] = None):
+    ase_filename = f"{basename}.aseprite"
+    json_filename = f"{basename}.json"
+    png_filename = f"{basename}.png"
+
+    # https://www.aseprite.org/docs/cli/
+    aseprite_args = [
+        "-b",
+        "--data", json_filename,
+        "--sheet", png_filename,
+        *(extra_args or []),
+        ase_filename,
+    ]
+
+    print(f"Exporting {ase_filename} to {json_filename} and {png_filename}...")
+    if subprocess.call([aseprite_binary, *aseprite_args], cwd=GRAPHICS_DIR):
+        # On Windows at least, this won't happen even if Aseprite fails,
+        # which is a bummer.
+        sys.exit(1)
 
 
 def find_aseprite() -> Path:
@@ -41,9 +47,12 @@ def find_aseprite() -> Path:
 
 
 def main():
-    print(f"Exporting {ASE_FILENAME} to {JSON_FILENAME} and {PNG_FILENAME}...")
-    if subprocess.call([find_aseprite(), *ASEPRITE_ARGS], cwd=GRAPHICS_DIR):
-        sys.exit(1)
+    aseprite = find_aseprite()
+    export(aseprite, 'manhattan', extra_args=[
+        "--split-layers",
+        "--filename-format", r"{layer}",
+    ])
+    export(aseprite, 'splash')
     print("Done.")
 
 
