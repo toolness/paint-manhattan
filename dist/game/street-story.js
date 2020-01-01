05af217aa3ec69cbd3669d1463aa1cd3411f37d0
Atul Varma <varmaa@gmail.com>
2020-01-01 09:13:21 -0500

    Rebuild JS.

--- dist/game/street-story.js
+++ dist/game/street-story.js
@@ -15,6 +15,36 @@ const STORIES = [
         content: "This street was given its name because it was one of three bridges that crossed a canal located at present-day Broad Street.",
         sourceURL: "https://en.wikipedia.org/wiki/Bridge_Street_(Manhattan)",
     },
+    {
+        name: "Front Street",
+        content: "This street was originally built on landfill in the latter half of the 18th century, and ran along the waterfront until the turn of the next century, when South street was built on more landfill.",
+        // Weirdly, the Wikipedia entry for "Front Street (Manhattan)" actually redirects to Lower Manhattan and includes no information about Front street.
+        sourceURL: "https://en.wikipedia.org/wiki/South_Street_(Manhattan)",
+    },
+    {
+        name: "Pearl Street",
+        content: [
+            "This street dates back to the early 1600s and was named for the many oysters found in the river. During British rule, it was called Great Queen Street.",
+            "It ran along the waterfront until the latter half of the 18th century, when Water and Front streets were built from landfill.",
+        ],
+        sourceURL: "https://en.wikipedia.org/wiki/Pearl_Street_(Manhattan)",
+    },
+    {
+        name: "Wall Street",
+        content: [
+            "From 1711 to 1762, at the corner of Wall and Pearl, the city operated its first official slave market for the sale and rental of enslaved Africans and Indians.",
+            "The city directly benefited from the sale of slaves by implementing taxes on every person who was bought and sold there.",
+        ],
+        sourceURL: "https://en.wikipedia.org/wiki/Wall_Street",
+    },
+    {
+        name: "Fair/Fulton Street",
+        content: [
+            "This street, along with Partition Street west of Broadway, was renamed to Fulton Street in 1816, in honor of Robert Fulton, the inventor of the steamship.",
+            "From 1882-1890, just south of Fulton and Pearl stood Pearl Street Station, the first commercial central power plant in the United States."
+        ],
+        sourceURL: "https://en.wikipedia.org/wiki/Fulton_Street_(Manhattan)",
+    }
 ];
 const STORY_CHARS_PER_LINE = 40;
 const STREET_NAME_Y = 15;
