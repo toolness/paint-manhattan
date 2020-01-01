e706280afdcfba5d5fea930172678479ea00afe7
Atul Varma <varmaa@gmail.com>
2020-01-01 15:38:49 -0500

    Persist debug form settings across session.

--- src/scripts/debug.ts
+++ src/scripts/debug.ts
@@ -2,6 +2,30 @@ import { loadAsepriteSheet } from "../aseprite-sheet.js";
 import { SPRITESHEET_URL } from "../game/urls.js";
 import { getStreetFrames } from "../game/sheet-frames.js";
 
+type FormControl = HTMLInputElement|HTMLSelectElement;
+
+function getAllFormControls(): FormControl[] {
+  return Array.from(document.querySelectorAll('input, select'));
+}
+
+function saveFormSetting(el: FormControl) {
+  if (el instanceof HTMLInputElement && el.type === 'checkbox') {
+    window.sessionStorage.setItem(el.name, el.checked ? 'YUP' : '');
+  } else {
+    window.sessionStorage.setItem(el.name, el.value);
+  }
+}
+
+function restoreFormSetting(el: FormControl) {
+  const val = window.sessionStorage.getItem(el.name);
+  if (val === null) return;
+  if (el instanceof HTMLInputElement && el.type === 'checkbox') {
+    el.checked = Boolean(val);
+  } else {
+    el.value = val;
+  }
+}
+
 async function debugMain() {
   const sheet = await loadAsepriteSheet(SPRITESHEET_URL);
   const streetNames = getStreetFrames(sheet);
@@ -19,6 +43,11 @@ async function debugMain() {
     optionEl.setAttribute('value', name);
     streetEl.appendChild(optionEl);
   }
+
+  getAllFormControls().forEach(el => {
+    restoreFormSetting(el);
+    el.addEventListener('change', () => saveFormSetting(el));
+  });
 }
 
 debugMain().catch(e => {
