1d7783019b606f77fb289901547d925a16379a33
Atul Varma <varmaa@gmail.com>
2020-01-02 18:54:23 -0500

    Rebuild JS.

--- dist/game/street-story.js
+++ dist/game/street-story.js
@@ -16,12 +16,12 @@ const STORIES = [
             "Built around 1725, George street was once named in honor of King George III, but eventually changed to Spruce after the Revolution.",
             "Today it's home to a famous skyscraper by Frank Gehry, located between William and Nassau.",
         ],
-        sourceURL: "https://en.wikipedia.org/wiki/Spruce_Street_(Manhattan)",
+        sources: ["https://en.wikipedia.org/wiki/Spruce_Street_(Manhattan)"],
     },
     {
         name: "Bridge Street",
         content: "Bridge street was given its name because it was one of three bridges that crossed a canal located at present-day Broad Street.",
-        sourceURL: "https://en.wikipedia.org/wiki/Bridge_Street_(Manhattan)",
+        sources: ["https://en.wikipedia.org/wiki/Bridge_Street_(Manhattan)"],
     },
     {
         name: "Front Street",
@@ -30,7 +30,7 @@ const STORIES = [
             "It ran along the waterfront until the turn of the next century, when a new road called South street was built from more landfill.",
         ],
         // Weirdly, the Wikipedia entry for "Front Street (Manhattan)" actually redirects to Lower Manhattan and includes no information about Front street.
-        sourceURL: "https://en.wikipedia.org/wiki/South_Street_(Manhattan)",
+        sources: ["https://en.wikipedia.org/wiki/South_Street_(Manhattan)"],
     },
     {
         name: "Pearl Street",
@@ -39,7 +39,7 @@ const STORIES = [
             "It ran along the waterfront until the latter half of the 18th century, when Water and Front streets were built from landfill.",
             "During British rule, it was called Great Queen Street, but changed back after the revolution.",
         ],
-        sourceURL: "https://en.wikipedia.org/wiki/Pearl_Street_(Manhattan)",
+        sources: ["https://en.wikipedia.org/wiki/Pearl_Street_(Manhattan)"],
     },
     {
         name: "Wall Street",
@@ -47,7 +47,7 @@ const STORIES = [
             "From 1711 to 1762, at the corner of Wall and Pearl, the city operated its first official market for the sale and rental of enslaved Africans and Indians.",
             "The city directly benefited from the sale of slaves by implementing taxes on every person who was bought and sold there.",
         ],
-        sourceURL: "https://en.wikipedia.org/wiki/Wall_Street",
+        sources: ["https://en.wikipedia.org/wiki/Wall_Street"],
     },
     {
         name: "Fair/Fulton Street",
@@ -55,7 +55,7 @@ const STORIES = [
             "Fair street, along with Partition Street west of Broadway, was renamed to Fulton Street in 1816, in honor of Robert Fulton, the inventor of the steamship.",
             "Eventually it extended to Pearl, and near their intersection in 1882 was built Pearl Street Station, the first commercial central power plant in the United States."
         ],
-        sourceURL: "https://en.wikipedia.org/wiki/Fulton_Street_(Manhattan)",
+        sources: ["https://en.wikipedia.org/wiki/Fulton_Street_(Manhattan)"],
     },
     {
         name: "Stone Street",
@@ -63,8 +63,10 @@ const STORIES = [
             "This was originally called Brewer Street because it was the location of the first commercial brewery in North America prior to 1646.",
             "Around 1655, it became the first street in the city to be paved with cobblestone, which earned it the name Stone Street."
         ],
-        sourceURL: "https://en.wikipedia.org/wiki/Stone_Street_(Manhattan)",
-        otherSources: [StorySource.Rogerson],
+        sources: [
+            "https://en.wikipedia.org/wiki/Stone_Street_(Manhattan)",
+            StorySource.Rogerson,
+        ],
     },
     {
         name: "New Street",
@@ -72,21 +74,22 @@ const STORIES = [
             "In 1679, when this street opened, a common designation for new streets until a better name was settled on was \"the new street\".",
             "However, a better name for this street was never settled on.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Chatham/Park Row",
         content: [
-            "Originally named after William Pitt, the Earl of Chatham and Prime Minister of England, this street was renamed Park Row by 1829 due to its location along City Hall Park.",
+            "Originally named after William Pitt, the Earl of Chatham and Prime Minister of England, Chatham Row was renamed Park Row by 1829 due to its location along City Hall Park.",
+            "By 1886, all of Chatham Street would be renamed Park Row as well.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Catharine Street",
         content: [
             "Catharine street was named after Catharine Desbrosses, a member of a prominent family whose distillery was located at the foot of this street.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Church Street",
@@ -94,7 +97,7 @@ const STORIES = [
             "Church Street was named for St. Paul's Chapel, which stands at what was originally the foot of the street at Partition/Fulton.",
             "In 1869, it was extended south to the Battery.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Beaver Street",
@@ -102,7 +105,7 @@ const STORIES = [
             "Beaver Street was initially laid out along a branch of the canal that existed in Broad Street long ago.",
             "It was named after the animal that was a prominent economic resource of New Amsterdam.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Broad Street",
@@ -110,7 +113,7 @@ const STORIES = [
             "In the early Dutch colony, a canal called the Heere Graft ran through the center of this street.",
             "The British filled the canal in 1676, resulting in a very wide street that became known as The Broad Street.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Vesey Street",
@@ -118,14 +121,14 @@ const STORIES = [
             "This street was named for the Reverend William Vesey, the first rector of Trinity Church.",
             "It was ceded by the church to the city in 1761.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Rector Street",
         content: [
             "This street, first laid out in 1739, was so named because the residence of the rector of Trinity Church stood here.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Broadway",
@@ -133,14 +136,14 @@ const STORIES = [
             "New Amsterdam had a large open area on the north side of its fort that formed the foot of \"De Breede Wegh\" which means \"The Broad Way\".",
             "The open area became modern Bowling Green Park, while The Broad Way extended to Wall Street, where one of two city gates was located.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Liberty Street",
         content: [
             "Originally called Crown Street, this street was renamed Liberty in 1794 to remove references to the nation's former colonial status.",
         ],
-        otherSources: [StorySource.Rogerson],
+        sources: [StorySource.Rogerson],
     },
     {
         name: "Ann Street",
@@ -148,7 +151,43 @@ const STORIES = [
             "This 3-block street appeared on city maps as early as 1728.",
             "In 1841, P.T. Barnum's American Museum opened at the corner of Ann and Vesey. It was was one of the most popular showplaces in the nation during the 19th century.",
         ],
-        sourceURL: "https://en.wikipedia.org/wiki/Ann_Street_(Manhattan)"
+        sources: ["https://en.wikipedia.org/wiki/Ann_Street_(Manhattan)"],
+    },
+    {
+        name: "Cliff Street",
+        content: [
+            "In 1796, the African Free School built a school house at 65 Cliff, near present-day Fulton.",
+            "It was founded to provide education to children of slaves and free people of color.",
+            "Its parent organization, the New York Manumission Society, fought to promote the gradual abolition of slavery.",
+        ],
+        sources: [
+            "https://en.wikipedia.org/wiki/African_Free_School",
+            "https://www.nyhistory.org/web/africanfreeschool/timeline/timeline-print.html",
+        ],
+    },
+    {
+        name: "Maiden Lane",
+        content: [
+            "This lane once ran along a stream leading to the East River. It was used by young women for washing clothes.",
+            "In 1712, over twenty enslaved Africans gathered in an orchard near Broadway and set fire to a building.",
+            "Bloodshed ensued, and strict laws were passed: slaves could no longer gather in groups of more than 3.",
+        ],
+        sources: [
+            StorySource.Rogerson,
+            "https://herb.ashp.cuny.edu/items/show/690",
+            "https://en.wikipedia.org/wiki/New_York_Slave_Revolt_of_1712",
+        ],
+    },
+    {
+        name: "Chatham Street/Park Row",
+        content: [
+            "In 1854, Elizabeth Jennings, a Black woman who taught at the African Free School, was forcibly ejected from a streetcar at Chatham and Pearl on account of the color of her skin.",
+            "She sued and won, which led to the eventual desegregation of all the city's transit systems by 1865."
+        ],
+        sources: [
+            "https://www.nytimes.com/2005/11/13/nyregion/thecity/the-schoolteacher-on-the-streetcar.html",
+            "https://en.wikipedia.org/wiki/Elizabeth_Jennings_Graham",
+        ],
     }
 ];
 const STORY_CHARS_PER_LINE = 35;
