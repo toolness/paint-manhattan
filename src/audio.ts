3bc5a2fc393a76168b7342fa0f01f7d2ce6246e4
Atul Varma <varmaa@gmail.com>
2020-01-04 10:37:49 -0500

    Make audio work offline.

--- src/audio.ts
+++ src/audio.ts
@@ -41,6 +41,20 @@ export function initializeAudio() {
 }
 
 async function loadAudio(url: string): Promise<HTMLAudioElement> {
+  // Browsers are extremely fidgety when it comes to loading audio, perhaps because
+  // they always assume it's going to be some massive file that it's going to need to
+  // stream or something (while our audio files are quite small).
+  //
+  // In any case, the caching of the audio for offline use, in particular, has lots
+  // of issues. See for example Google Workbox's documentation on serving cached
+  // audio and video:
+  //
+  //   https://developers.google.com/web/tools/workbox/guides/advanced-recipes#cached-av
+  //
+  // It seems we can work around all this jibberjabber by just retrieving the actual
+  // bytes of audio data (which works consistently offline) and then creating an object URL
+  // from it, which we point an <audio> element at.
+
   if (!url.endsWith('.mp3')) {
     throw new Error('Only MP3 audio is currently supported!');
   }
