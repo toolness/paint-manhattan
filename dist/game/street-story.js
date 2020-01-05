f53c1e8e61b159dbb5fae0caff2017637771a0ec
Atul Varma <varmaa@gmail.com>
2020-01-04 19:20:35 -0500

    Rebuild JS.

--- dist/game/street-story.js
+++ dist/game/street-story.js
@@ -230,32 +230,11 @@ const MS_PER_CHAR = 40;
 export class StreetStoryState extends ManhattanState {
     constructor(game, gameplayState, story) {
         super(game);
-        this.game = game;
         this.gameplayState = gameplayState;
         this.story = story;
         this.storyLines = paragraphsToWordWrappedLines(story.content, STORY_CHARS_PER_LINE);
-        this.charsToAnimate = this.storyLines.reduce((total, line) => total + line.length, 0);
-        const timer = new Timer(MS_PER_CHAR, game.updateAndDraw);
-        this.subState = { type: 'animating', timer };
-        timer.start();
     }
-    update() {
-        const { game, subState } = this;
-        if (subState.type === 'waiting_for_user') {
-            if (game.pen.justWentUp) {
-                game.changeState(this.gameplayState);
-            }
-        }
-        else if (subState.type === 'animating') {
-            if (subState.timer.tick >= this.charsToAnimate || game.pen.justWentUp) {
-                subState.timer.stop();
-                const prompt = new ActionPrompt(game, 'to continue');
-                this.subState = { type: 'waiting_for_user', prompt };
-                prompt.start();
-            }
-        }
-    }
-    draw(ctx) {
+    drawStory(ctx, storyLines) {
         const { game } = this;
         const { font: big, tinyFont: small } = game.options;
         const { width } = this.game.canvas;
@@ -264,19 +243,29 @@ export class StreetStoryState extends ManhattanState {
         const streetName = shortenStreetName(this.story.name).toUpperCase();
         big.drawText(ctx, streetName, centerX, STREET_NAME_Y, 'center');
         let currY = STREET_STORY_Y;
-        for (let line of this.getAnimatingStoryLines()) {
+        for (let line of storyLines) {
             small.drawText(ctx, line, centerX, currY, 'center');
             currY += small.options.charHeight;
         }
-        if (this.subState.type === 'waiting_for_user') {
-            this.subState.prompt.draw(ctx);
-        }
+    }
+    static forStreet(game, gameplayState, streetName) {
+        const story = getStreetStory(streetName);
+        if (!story)
+            return null;
+        return new AnimatingSubState(game, gameplayState, story);
+    }
+    static existsForStreet(streetName) {
+        return getStreetStory(streetName) !== null;
+    }
+}
+class AnimatingSubState extends StreetStoryState {
+    constructor(game, gameplayState, story) {
+        super(game, gameplayState, story);
+        this.charsToAnimate = this.storyLines.reduce((total, line) => total + line.length, 0);
+        this.timer = new Timer(MS_PER_CHAR, game.updateAndDraw);
     }
     getAnimatingStoryLines() {
-        if (this.subState.type === 'waiting_for_user') {
-            return this.storyLines;
-        }
-        const maxChars = this.subState.timer.tick;
+        const maxChars = this.timer.tick;
         const lines = [];
         let chars = 0;
         for (let line of this.storyLines) {
@@ -293,20 +282,40 @@ export class StreetStoryState extends ManhattanState {
         }
         return lines;
     }
-    exit() {
-        if (this.subState.type !== 'waiting_for_user') {
-            throw new Error('Assertion failure, we should only exit the state when waiting for user!');
+    draw(ctx) {
+        this.drawStory(ctx, this.getAnimatingStoryLines());
+    }
+    update() {
+        if (this.timer.tick >= this.charsToAnimate || this.game.pen.justWentUp) {
+            this.game.changeState(new WaitingForUserSubState(this.game, this.gameplayState, this.story));
         }
-        this.subState.prompt.stop();
     }
-    static forStreet(game, gameplayState, streetName) {
-        const story = getStreetStory(streetName);
-        if (!story)
-            return null;
-        return new StreetStoryState(game, gameplayState, story);
+    enter() {
+        this.timer.start();
     }
-    static existsForStreet(streetName) {
-        return getStreetStory(streetName) !== null;
+    exit() {
+        this.timer.stop();
+    }
+}
+class WaitingForUserSubState extends StreetStoryState {
+    constructor(game, gameplayState, story) {
+        super(game, gameplayState, story);
+        this.prompt = new ActionPrompt(game, 'to continue');
+    }
+    draw(ctx) {
+        this.drawStory(ctx, this.storyLines);
+        this.prompt.draw(ctx);
+    }
+    update() {
+        if (this.game.pen.justWentUp) {
+            this.game.changeState(this.gameplayState);
+        }
+    }
+    enter() {
+        this.prompt.start();
+    }
+    exit() {
+        this.prompt.stop();
     }
 }
 function getStreetStory(streetName) {
