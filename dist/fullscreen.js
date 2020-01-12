68ff9be0b00a3b95436502443c3d311d164a2ac2
Atul Varma <varmaa@gmail.com>
2020-01-11 20:07:37 -0500

    Remove compiled JS.

--- dist/fullscreen.js
+++
@@ -1,8 +0,0 @@
-export function canSupportFullscreen() {
-    return !!document.documentElement.requestFullscreen;
-}
-export function requestFullscreen() {
-    if (canSupportFullscreen()) {
-        document.documentElement.requestFullscreen();
-    }
-}
