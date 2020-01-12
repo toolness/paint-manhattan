65cb9d26035ee67e94985d6a19f8aa925d97147a
Atul Varma <varmaa@gmail.com>
2020-01-11 19:42:51 -0500

    Move build scripts to build/, use two tsconfigs.

---
+++ build/build-metadata.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+
+/**
+ * Globs needed for running the game offline. Specifically,
+ * these are the files that will be cached by the game's
+ * service worker.
+ */
+export const OFFLINE_GLOBS = [
+  'graphics/**/*.png',
+  'graphics/**/*.json',
+  'vendor/**/*',
+  'dist/**/*.js',
+  'audio/**/*.mp3',
+  'favicon.ico',
+  'index.html',
+  'debug.html',
+];
+
+/** The game's service worker. */
+export const SERVICE_WORKER_FILENAME = 'service-worker.js';
+
+/**
+ * Globs needed to distribute the game, but not needed for
+ * offline operation.
+ */
+export const ONLINE_GLOBS = [
+  'icons/**/*.png',
+  'manifest.json',
+  'thumbnail.png',
+  SERVICE_WORKER_FILENAME,
+];
