5553cf6eb6341009402fd0a3e951ea460ba66e09
Atul Varma <varmaa@gmail.com>
2020-01-20 08:18:41 -0500

    Updates

--- dist/offline.js
+++ dist/offline.js
@@ -17,7 +17,7 @@ export function wantsOfflineSupport() {
     // Note that the service worker script uses the same logic to determine
     // if the requesting/requested page wants offline support, so if this
     // logic changes, make sure it changes in the service worker too.
-    return qs.get('offline') === 'on';
+    return qs.get('nooffline') !== 'on';
 }
 /** Returns whether the browser can support offline mode. */
 export function canSupportOffline() {
